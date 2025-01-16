c4a5c2c6351aa0cd366962929882df0024fe4048
Razvan Sfirlogea <razvan.sfirlogea@gmail.com>
2025-01-16 14:58:36 +0200

    second commit

--- src/ui/Cards.js
+++ src/ui/Cards.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import Arrowright from './icons/Arrowright';
 import Data from './Data.json'
+import Categories from './Categories.json'
+
+function getData() {
+    return Data.map((object) => {
+        const matched = Categories.find((element) => element.id === object.id)
+        return { ...object, ...matched }
+    });
+}
 
 export default function Cards() {
     return (
@@ -13,24 +21,22 @@ export default function Cards() {
                 </div>
             </div>
             <div className="card-row">
-                {Data.map((item) => {
-                    return (
-                        <div className="card">
-                            <div className="img" style={{ backgroundImage: item.image }}></div>
-                            <div className="content">
-                                <div className="card-title">{item.title}</div>
-                                <div className="author">{item.author}</div>
-                                <div className="desc"><p>{item.desc}</p></div>
-                                <div className="end h2">
-                                    <div className="btn-link">
-                                        Get Started
-                                        <Arrowright />
-                                    </div>
+                {getData().map((card) => (
+                    <div className="card" key={card.id}>
+                        <div className="img" style={{ backgroundImage: card.image }}></div>
+                        <div className="content">
+                            <div className="card-title">{card.title}</div>
+                            <div className="author">{card.author}</div>
+                            <div className="desc"><p>{card.desc}</p></div>
+                            <div className="end h2">
+                                <div className="btn-link">
+                                    Get Started
+                                    <Arrowright />
                                 </div>
                             </div>
                         </div>
-                    )
-                })}
+                    </div>
+                ))}
             </div>
         </div>
     )
--- src/ui/Menu.js
+++ src/ui/Menu.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Dashboard from './icons/Dashboard'
 import Library from './icons/Library'
 import Paths from './icons/Paths'
 
 export default function Menu() {
+  const [hover, setHover] = useState(false);
   return (
-    <div className="Menu" id="menu">
-      <div className="item">
+    <div className="Menu">
+      <div className={`item ${hover ? "active" : ""}`}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
         <Dashboard />
         Dashboard
       </div>
-      <div className="item">
+      <div className={`item ${hover ? "active" : ""}`}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
         <Library />
         Library
       </div>
-      <div className="item">
+      <div className={`item ${hover ? "active" : ""}`}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
         <Paths />
         My Paths
       </div>
